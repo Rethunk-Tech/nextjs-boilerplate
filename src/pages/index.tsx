4ffa46dc878c7de82b0c540dc1206213fc7ab842
Damon Blais <damon@rethunk.tech>
2023-01-06 03:53:55 -0800

    :recycle: More Complete Index Example

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,23 +1,87 @@
 import HomeRounded from '@mui/icons-material/HomeRounded'
-import Container from '@mui/material/Container'
+import Box from '@mui/material/Box'
 import Typography from '@mui/material/Typography'
+import Footer from 'components/example/Footer'
+import Link from 'components/mui/Link'
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import Link from 'next/link'
+
+const styles = {
+  container: {
+    display:    'grid',
+    height:     '100vh',
+    placeItems: 'center',
+    textAlign:  'center',
+  },
+
+  typography: {
+    my: 4,
+
+    // no marinBottom on last of type
+    '&:last-of-type': {
+      mb: 0,
+    },
+
+    // link
+    '& a': {
+      textDecoration: 'none',
+    },
+
+    // code
+    '& code': {
+      border:       '1px solid',
+      borderColor:  'secondary.light',
+      borderRadius: 1,
+      color:        'secondary.main',
+      display:      'inline-block',
+      px:           1,
+      py:           0.5,
+    },
+  },
+}
+
 
 const Home: NextPage = (): JSX.Element => {
   return <>
     <Head>
-      <title>Create Next App</title>
+      <title>Boilerplate by Rethunk.Tech</title>
     </Head>
 
-    <Container maxWidth="lg" sx={{ my: 12, textAlign: 'center' }}>
-      <Typography component="h1" variant="h1" sx={{ mb: 6 }}>
-          Welcome to <Link href="https://nextjs.org">Next.js!</Link>
-      </Typography>
+    {/* full-height container with centered content */}
+    <Box sx={styles.container}>
+      {/* inner container holds centered content together */}
+      <Box>
+        <HomeRounded sx={{ fontSize: '14rem' }} />
 
-      <HomeRounded fontSize="inherit" sx={{ fontSize: '14rem' }} />
-    </Container>
+        <Typography component="h1" sx={styles.typography} variant="h2">
+          <Link
+            href="https://nextjs.org"
+            rel="noopener noreferrer"
+            target="_blank"
+          >
+            Next.js
+          </Link> + <Link
+            href="https://mui.com"
+            rel="noopener noreferrer"
+            target="_blank"
+          >
+            MUI
+          </Link> + <Link
+            href="https://typescriptlang.org"
+            rel="noopener noreferrer"
+            target="_blank"
+          >
+            TypeScript
+          </Link>!
+        </Typography>
+
+        <Typography component="p" sx={styles.typography} variant="h4">
+          Get started by editing <code>pages/index.tsx</code>
+        </Typography>
+      </Box>
+    </Box>
+
+    <Footer />
   </>
 }
 
