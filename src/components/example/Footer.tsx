e9c934f4130080233be0fbbea55c59c949e6bc23
Damon Blais <damon@rethunk.tech>
2023-04-10 11:44:52 -0700

    🎨 Consistent use of `export default function`
    Signed-off-by: Damon Blais <damon@rethunk.tech>

--- src/components/example/Footer.tsx
+++ src/components/example/Footer.tsx
@@ -1,19 +1,18 @@
 import Box from '@mui/material/Box'
 import Link from 'components/mui/Link'
-import type { FC } from 'react'
 
 const styles = {
   footer: {
     bottom:    0,
     left:      0,
-    right:     0,
     m:         4,
     position:  'fixed',
+    right:     0,
     textAlign: 'center',
   },
 }
 
-const Footer: FC = (): JSX.Element => {
+export default function Footer(): JSX.Element {
   return <Box sx={styles.footer}>
     Made with ❤️ by <Link
       href="https://we.rethunk.tech/"
@@ -24,5 +23,3 @@ const Footer: FC = (): JSX.Element => {
     </Link>
   </Box>
 }
-
-export default Footer
