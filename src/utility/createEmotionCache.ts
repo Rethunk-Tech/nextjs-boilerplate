ddb806d1fd7a59aea28c5b81f2e387b75a5a2109
Damon Blais <damon@rethunk.tech>
2024-08-03 02:26:47 -0700

    refactor(utility): remove unused createEmotionCache utility file

--- src/utility/createEmotionCache.ts
+++
@@ -1,8 +0,0 @@
-import createCache from '@emotion/cache'
-import type { EmotionCache } from '@emotion/utils'
-
-const createEmotionCache: () => EmotionCache = (): EmotionCache => {
-  return createCache({ key: 'css' })
-}
-
-export default createEmotionCache
